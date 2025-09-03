98c6526064412dea260fab0dd1e01b0e0b444719
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-03 12:03:15 -0300

    catalogo gamer

--- frontend/src/mocks/gamesMocks.jsx
+++ frontend/src/mocks/gamesMocks.jsx
@@ -4,21 +4,21 @@ export const gamesMock = [
     id: 1,
     titulo: "The Last of Us Part I",
     descricao: "Um jogo de ação e aventura emocionante.",
-    capaRetrato: "tlou1-image.png",
-    capaPaisagem: "tlou1-paisagem.png"
+    imagem_url: "tlou1-image.png",
+    imagem_paisagem: "tlou1-paisagem.png"
   },
   {
     id: 2,
     titulo: "God of War Ragnarok",
     descricao: "A batalha de Kratos e Atreus contra os deuses nórdicos.",
-    capaRetrato: "gow-image.png",
-    capaPaisagem: "gow-paisagem.png"
+    imagem_url: "gow-image.png",
+    imagem_paisagem: "gow-paisagem.png"
   },
   {
     id: 3,
     titulo: "Elden Ring",
     descricao: "Um vasto mundo aberto com desafios insanos.",
-    capaRetrato: "pubg-image.png",
-    capaPaisagem: "pubg-paisagem.png"
+    imagem_url: "pubg-image.png",
+    imagem_paisagem: "pubg-paisagem.png"
   }
 ]
