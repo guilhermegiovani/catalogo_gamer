0e65349d1125ec112ac5343e688b31a9c69c185a
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-03 12:05:13 -0300

    catalogo gamer

--- frontend/src/mocks/authMock.jsx
+++ frontend/src/mocks/authMock.jsx
@@ -1,7 +1,8 @@
 export const userMock = {
   id: 1,
   nome: "Guilherme",
-  email: "guilherme@example.com",
+  email: "guilherme@email.com",
+  senha: 123789,
   role: "admin", // ou "user"
   foto_perfil: "/avatar.png", // pode ser uma imagem no /public
   token: "fake-jwt-token-123",
