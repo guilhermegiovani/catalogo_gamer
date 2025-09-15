b06bc7c7d0199a66b8d57164c23b953f89742c29
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-15 16:26:59 -0300

    teste

--- frontend/src/pages/ReviewGame.jsx
+++ frontend/src/pages/ReviewGame.jsx
@@ -28,8 +28,9 @@ function ReviewGame() {
             setReviewsData(res.data.reviews)
             
             const resAvg = await getReviewsAvgs()
-            const avg = resAvg.data.filter(avg => avg.gameId === gameId)
+            const avg = resAvg.data.find(avg => avg.gameId === gameId)
             console.log(resAvg.data)
+            console.log(avg)
             setAvgGame(avg)
 
 
