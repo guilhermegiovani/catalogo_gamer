823c4c52f4c5b83bccfbd560cd5b7513c8e45093
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-22 11:43:41 -0300

    test

--- backend/utils/resend.js
+++ backend/utils/resend.js
@@ -1,5 +1,7 @@
 import { Resend } from "resend";
 import dotenv from "dotenv"
+
+const envFile = process.env.NODE_ENV === "production" ? ".env.production" : ".env.development"
 dotenv.config({ path: envFile })
 
 console.log(`Resend: ${process.env.RESEND_API_KEY}`)
