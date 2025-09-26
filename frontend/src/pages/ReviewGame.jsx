0a40c2949e20657771241dbb51a09f2e166a638c
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-26 17:37:28 -0300

    test

--- frontend/src/pages/ReviewGame.jsx
+++ frontend/src/pages/ReviewGame.jsx
@@ -273,7 +273,7 @@ function ReviewGame() {
                                                         />}
                                                         handleClick={() => fetchLike(rev.id)}
                                                     />
-                                                    {/* <p>{reactions[rev.id].likesReview}</p> */}
+                                                    <p>{reactions[rev.id].likesReview}</p>
                                                     {/* {keysReactions.map((k) => (
                                                         <p>{}</p>
                                                     ))} */}
@@ -289,7 +289,7 @@ function ReviewGame() {
                                                         />}
                                                         handleClick={() => fetchDisLike(rev.id)}
                                                     />
-                                                    {/* <p>{reactions[rev.id].dislikesReview}</p> */}
+                                                    <p>{reactions[rev.id].dislikesReview}</p>
                                                 </div>
                                             </div>
                                         )
