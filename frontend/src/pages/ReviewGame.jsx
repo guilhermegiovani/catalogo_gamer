cb168e5d0198866b0178b66eacbf6cc3dd37eedd
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-29 11:12:08 -0300

    test

--- frontend/src/pages/ReviewGame.jsx
+++ frontend/src/pages/ReviewGame.jsx
@@ -273,7 +273,7 @@ function ReviewGame() {
                                                             size={20} fill="currentColor"
                                                             className={clsx(
                                                                 "text-white/20 -mt-[1px] cursor-pointer hover:text-blue-500/30 transition duration-200",
-                                                                userReactions[rev.id] === "like" ? "text-blue-500/30" : ""
+                                                                userReactions[rev.id] === "like" ? "text-blue-500/30" : "text-purple-500/30"
                                                             )}
                                                         />}
                                                         handleClick={() => fetchLike(rev.id)}
@@ -290,7 +290,7 @@ function ReviewGame() {
                                                             size={20} fill="currentColor"
                                                             className={clsx(
                                                                 "text-white/20 -mt-[1px] cursor-pointer hover:text-red-500/30 transition duration-200",
-                                                                userReactions[rev.id] === "dislike" ? "text-red-500/30" : ""
+                                                                userReactions[rev.id] === "dislike" ? "text-red-500/30" : "text-green-500/30"
                                                             )}
                                                         />}
                                                         handleClick={() => fetchDisLike(rev.id)}
