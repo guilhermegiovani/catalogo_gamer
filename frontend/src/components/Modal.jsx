a81be630c71270180115a8f4faf4d2cd5983754f
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-09-30 16:53:56 -0300

    Catálogo Gamer

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -21,19 +21,21 @@ function Modal({ isOpen, onClose, onConfirm, title, message }) {
                         exit={{ scale: 0.9, opacity: 0 }}
                         transition={{ duration: 0.2, ease: "easeOut" }}
                         className={clsx(
-                            "bg-white rounded-2xl shadow-lg p-6 w-full max-w-md",
-                            "flex flex-col gap-4"
+                            "bg-[#1a1a2e] rounded-2xl shadow-lg p-6 w-full max-w-md",
+                            "flex flex-col gap-4",
+                            "shadow-[0_0_20px_2px_rgba(59,59,92,0.6)]"
                         )}
                     >
-                        <h2 className="text-xl font-semibold text-gray-900">{title}</h2>
-                        <p className="text-gray-600">{message}</p>
+                        <h2 className="text-xl font-semibold text-white">{title}</h2>
+                        <p className="text-[#b0b0c3]">{message}</p>
 
                         <div className="flex justify-end gap-2 mt-4">
                             <Button
                                 text="Cancelar"
                                 className={clsx(
-                                    "px-4 py-2 rounded-xl font-medium",
-                                    "bg-gray-200 hover:bg-gray-300 text-gray-700"
+                                    "py-2 rounded-md font-semibold",
+                                    "bg-[#3b3b5c] hover:bg-[#4c4c70]",
+                                    "text-gray-300 cursor-pointer xl:text-lg"
                                 )}
                                 handleClick={onClose}
                             />
@@ -60,8 +62,9 @@ function Modal({ isOpen, onClose, onConfirm, title, message }) {
                             <Button
                                 text="Deletar"
                                 className={clsx(
-                                    "px-4 py-2 rounded-xl font-medium",
-                                    "bg-red-600 hover:bg-red-700 text-white"
+                                    "py-2 rounded-md font-semibold cursor-pointer",
+                                    "bg-red-600 hover:bg-red-700 text-gray-300 xl:text-lg",
+                                    "hover:brightness-130"
                                 )}
                                 handleClick={onConfirm}
                             />
