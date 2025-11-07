b1495ef9c9a8f126488fe629e5c9ea9f6f191ea3
Guilherme Nobre <146262450+guilhermegiovani@users.noreply.github.com>
2025-11-07 16:12:51 -0300

    Ajustando footer mobile

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,10 @@ function Footer() {
 
     return (
         <footer className={clsx(
-            "w-full absolute z-10 py-3 bg-[#0f0f2b]",
-            "flex justify-center", // [#0f0f2b]
-            "border border-[#2d2d5f]"
+            "w-full absolute z-10 py-2 lg:py-3 bg-[#0f0f2b]",
+            "flex justify-center",
+            "border border-[#2d2d5f]",
+            "text-xs lg:text-base"
         )}>
             <p>
                 © 2025 Catálogo Gamer · Desenvolvido por 
